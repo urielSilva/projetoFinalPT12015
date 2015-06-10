80cec6bf047519a7f1ada92f8eea7eeb37032887
eduardoaraujocunha <eduardoaraujocunhaa@gmail.com>
2015-06-10 16:20:00 -0300

    Correção! Entidade cargos criada!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610003007) do
+ActiveRecord::Schema.define(version: 20150610044332) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "name_job",   limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "meetings", force: :cascade do |t|
     t.string   "name_meeting",        limit: 255
