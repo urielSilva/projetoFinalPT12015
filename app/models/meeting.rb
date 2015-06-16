67497c080c7c677483fa2aabc0c948c77eefb9d8
Capotinh0 <rafa197795@gmail.com>
2015-06-16 20:18:48 -0300

    Ajuste nas views de Membros e Reunioes, para mostrar o nome das FK's e nao os id's

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,4 +2,13 @@ class Meeting < ActiveRecord::Base
 
   has_one :agenda
 
+  def teste
+    @agendas = Agenda.all
+  end
+
+  def pega_nome (id_pesq)
+    teste
+    @agendas.find(id_pesq).agenda_name
+  end
+
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,13 @@ class Member < ActiveRecord::Base
 
   has_one :job
 
+  def teste
+    @jobs = Job.all
+  end
+
+  def pega_job(id_pesq)
+    teste
+    @jobs.find(id_pesq).job_name
+  end
+
 end
