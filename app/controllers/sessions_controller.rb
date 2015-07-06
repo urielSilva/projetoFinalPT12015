7984f2acb3f064008e5f96636dd2915c2c30132b
Capotinh0 <rafa197795@gmail.com>
2015-07-05 21:01:26 -0300

    Ajuste no Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,21 @@ class SessionsController < ApplicationController
   end
  
   def create
-  	user=Member.find_by(member_email: params[:session][:email])
-  	if user && user.auth(params[:session][:password])
-  		log_in user
-  		redirect_to user
-  	else
-  		flash.now[:danger] = 'Email/senha invalidos'
-  		render 'new'
-  	end
+    if logged_in?
+    flash.now[:danger] = 'Faça o logout antes de logar com um usuário diferente'
+      render 'new'
+    else
+      user=Member.find_by(member_email: params[:session][:email])
+      if user && user.auth(params[:session][:password])
+        log_in user
+        redirect_to user
+      else
+        flash.now[:danger] = 'Email/senha invalidos'
+        render 'new'
+      end
+    end
   end
+
 
   def destroy
     log_out if logged_in?
